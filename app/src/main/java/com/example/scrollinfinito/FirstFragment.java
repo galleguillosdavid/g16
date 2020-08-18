2f28414c73842626333f56e27ea7e03de746a2c7
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-08-17 23:00:30 -0400

    Listo, devuelve un toast con el nombre de lo seleccionado al hacer click

--- app/src/main/java/com/example/scrollinfinito/FirstFragment.java
+++ app/src/main/java/com/example/scrollinfinito/FirstFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,7 +18,7 @@ import com.example.scrollinfinito.databinding.FragmentFirstBinding;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements WordAdapter.PassElementSelected {
     private FragmentFirstBinding binding;
     private RecyclerView mRecyclerView;
     private WordAdapter mWordAdapter;
@@ -32,7 +33,7 @@ public class FirstFragment extends Fragment {
         binding = FragmentFirstBinding.inflate(inflater, container, false);
 
         mRecyclerView = binding.myRecycler;
-        mWordAdapter = new WordAdapter(getWordData());
+        mWordAdapter = new WordAdapter(getWordData(),this);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         mRecyclerView.setAdapter(mWordAdapter);
 
@@ -58,5 +59,10 @@ public class FirstFragment extends Fragment {
             palabras.add("WORD " + i);
         }
         return palabras;
+    }
+
+    @Override
+    public void passElement(String word) {
+        Toast.makeText(getContext(), word, Toast.LENGTH_SHORT).show();
     }
 }
